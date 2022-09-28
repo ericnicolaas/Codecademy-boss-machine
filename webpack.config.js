f83c2f97ea9a4cf7d3b9dc74c1b8075451b5770e
Eric Daams <eric@164a.com>
2022-09-28 10:18:07 +0930

    Initial commit. This is the basic state of the project initially as downloaded from Codecademy, but with dependencies updated to the latest version.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+
+module.exports = {
+  entry: './browser/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/js/bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        }
+      }
+    ]
+  }
+};
